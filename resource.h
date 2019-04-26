2d46c54d7a17307fb45d07a95b37b46dc3d82e45
3eSun <49555408+3eSun@users.noreply.github.com>
2019-04-26 13:45:36 +0800

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by teluosi.rc
+//
+#define ID_INTER                        0
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_TELUOSTYPE                  129
+#define IDD_DIALOG1                     130
+#define IDD_DIALOG2                     131
+#define IDD_DIALOG3                     132
+#define IDB_BITMAP1                     133
+#define IDB_BITMAP2                     134
+#define IDB_BITMAP3                     135
+#define IDB_BITMAP4                     136
+#define IDB_BITMAP5                     137
+#define IDB_BITMAP6                     138
+#define IDB_BITMAP7                     139
+#define IDB_BITMAP8                     140
+#define IDB_BITMAP9                     141
+#define IDC_EDIT1                       1000
+#define ID_MENU_START                   32771
+#define ID_MENU_DSTART                  32772
+#define ID_MENU_TSTART                  32773
+#define ID_VIEW_1                       32774
+#define ID_VIEW_2                       32775
+#define ID_MENU_PAUSE                   32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        145
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
